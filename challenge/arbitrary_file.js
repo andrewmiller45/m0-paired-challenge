aa4239a34375c3bab8aad0546c408e20fccbbe09
Andrew Miller <andrew.r.miller45@gmail.com>
2022-02-09 19:24:57 -0800

    add partner mini project

---
+++ challenge/arbitrary_file.js
@@ -0,0 +1,10 @@
+var email ="andy@email.com";
+var age = 30;
+var isAClimber= true;
+var month ="February";
+var zipCode = "98199";
+var colorFavorite = "Green";
+//
+var genres = ['Non-Fiction', 'Fiction', "Sci-Fi", "Young Adult", "Cooking","Poetry"];
+// + Assigned book genres to an array.
+console.log(genres);
---
+++ challenge/index.md
@@ -0,0 +1,12 @@
+#Book Titles
+1. The Tangled Tree
+2. The Case Against Reality
+3. Javascript & JQuery
+4. Climbed Injuries Solved
+5. Watchmen
+
+#Workflow
++ Both of us drove at different parts.
++ Both navigated.
++ We did get stuck on `git clone`
+s 
